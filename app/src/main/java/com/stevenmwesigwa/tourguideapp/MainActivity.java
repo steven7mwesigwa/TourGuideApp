c3e4f75454730369753365452f48a71b85defacf
Steven Mwesigwa <contact@stevenmwesigwa.com>
2019-11-12 07:07:57 +0300

    Completed designing the UI of navigation drawer -LAYOUT AND HAMBURGER ICON

--- app/src/main/java/com/stevenmwesigwa/tourguideapp/MainActivity.java
+++ app/src/main/java/com/stevenmwesigwa/tourguideapp/MainActivity.java
@@ -1,9 +1,10 @@
 package com.stevenmwesigwa.tourguideapp;
 
-import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        // Tell your app that you
+        // Tell your app that you want to use your own toolbar instead
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
@@ -24,11 +25,18 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        toggle.syncState(); // Takes care of rotating the hamburger icon
     }
 
     @Override
     public void onBackPressed() {
+        /*
+         * Because when we press the back button when our navigation drawer is open
+         * we don't want to leave the Activity immediately, Instead we want to close
+         * our navigation drawer.
+         * If the drawer were on the "right side" of the screen we would have used
+         * drawer.closeDrawer(GravityCompat.END); instead to close it.
+         */
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
